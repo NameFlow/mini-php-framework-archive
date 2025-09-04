5cb88f785cdc5118071e39b66e846f3252bdcdec
ForgeAxiom <forgeaxiom@proton.me>
2025-09-04 03:19:40 +0300

    chore: connection documented

--- src/Database/Connection/Connection.php
+++ src/Database/Connection/Connection.php
@@ -3,19 +3,34 @@ declare(strict_types=1);
 
 namespace App\Database\Connection;
 
-
 use PDO;
 
+/**
+ * Statement service, responsible for PDO DB connection
+ */
 final class Connection
 {
     public readonly PDO $pdo;
 
+    /**
+     * @param string $dbPath Where database located.
+     */
     public function __construct(string $dbPath)
     {
         $this->pdo = new PDO('sqlite:' . $dbPath);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
+    /**
+     * Prepares a statement for execution.
+     *
+     * Prepares an SQL statement for execution, returning a statement handle.
+     *
+     * @param string $sql The SQL statement to prepare.
+     *
+     * @return Statement Returns a PDOStatement object or throws an exception on failure.
+     * @throws \PDOException If the SQL statement is invalid or cannot be prepared.
+     */
     public function prepare(string $sql): Statement
     {
         return new Statement($this->pdo->prepare($sql));
